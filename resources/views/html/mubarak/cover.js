dd17c0a8e1fa5104712f1c82688990ac8547c5ca
mammubarak <mubarakakram1234@gmail.com>
2021-06-26 01:23:41 +0530

    responsive for 360px display

--- resources/views/html/mubarak/cover.js
+++ resources/views/html/mubarak/cover.js
@@ -1,10 +1,4 @@
-  
-let container = document.getElementById('container')
 
-toggle = () => {
-	container.classList.toggle('sign-in')
-	container.classList.toggle('sign-up')
-}
 
 /*setTimeout(() => {
 	container.classList.add('sign-in')
